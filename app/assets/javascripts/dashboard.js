638bbdcafb6478fd6bdb06eddd75dc9ed2647124
david riley <dtriley4@gmail.com>
2015-04-15 23:52:10 -0400

    plotting route between origin and destination

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -12,15 +12,21 @@ $(document).on('ready page:load', function() {
 
     console.log(origin, destination, date, time, speed, timeInterval);
 
+    // Empty out array.
+    forecastApp.flightPathCoordinates = [];
+
     forecastApp.getResolvedLocation(origin)
     forecastApp.getResolvedLocation(destination)
   };
 
-
   // Resolve
   forecastApp.getResolvedLocation = function(locationQuery) {
     $.getJSON('resolve/' + locationQuery, function(data) {
-      console.log(data);
+      forecastApp.flightPathCoordinates.push(new google.maps.LatLng(data.location[0], data.location[1]));
+
+      if(forecastApp.flightPathCoordinates.length === 2) {
+        initializeMap();
+      }
     });
   };
 
@@ -32,12 +38,25 @@ $(document).on('ready page:load', function() {
   // Map
   var initializeMap = function() {
     var mapOptions = {
-      center: { lat: -34.397, lng: 150.644}, zoom: 8
+      center: { lat: 35.877639, lng: -78.787472}, zoom: 4
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var map = forecastApp.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    if(forecastApp.flightPathCoordinates) {
+      var flightPath = new google.maps.Polyline({
+        path: forecastApp.flightPathCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+
+      flightPath.setMap(map);
+    }
+
   };
 
-  initialize();
+  initializeMap();
 
   $('.submit-flight').click(forecastApp.submitFlightForm);
 });
