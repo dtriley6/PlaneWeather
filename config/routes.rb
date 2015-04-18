a13db1881664c6275a6adb23fbddb218d82b9c9e
david riley <dtriley4@gmail.com>
2015-04-18 14:36:35 -0400

    change api endpoint to accept json as default

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  match '/resolve/:location', to: 'location#resolve', via: 'get', constraints: { location: /[^\/^json]+/ }
+  match '/resolve/:location', to: 'location#resolve', via: 'get', constraints: { location: /[^\/^json]+/ }, defaults: {format: :json}
 
   match '/forecast/:origin/:destination/:departure/:speed/:interval', to: 'forecast#calculate',
-    via: 'get', constraints: { origin: /[^\/^json]+/, destination: /[^\/^json]+/, departure: /.*/ }
+    via: 'get', constraints: { origin: /[^\/^json]+/, destination: /[^\/^json]+/, departure: /.*/ }, defaults: {format: :json}
    
 
   root 'dashboard#home'
